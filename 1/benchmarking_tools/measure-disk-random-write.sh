ea41edc82b25eab337cf6e461304ef63a542a68f
Youri K <youri.kaminsky@student.hpi.de>
2019-11-17 15:45:06 +0100

    update script for fio version 3.1

--- 1/benchmarking_tools/measure-disk-random-write.sh
+++ 1/benchmarking_tools/measure-disk-random-write.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-result=$(fio --rw=randwrite --name=rand_write_test --size=20M --direct=1 --bs=1024k --numjobs=4 --group_reporting --runtime=2 --time_based | head -7 | tail -1 | cut -d "," -f 3 | cut -c 7-)
+result=$(fio --rw=randwrite --name=rand_write_test --size=20M --direct=1 --bs=1024k --runtime=10 --time_based | head -16 | tail -1 | cut -d "," -f 3 | cut -d "=" -f 2)
 echo $result
