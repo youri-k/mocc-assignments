86b3fe104f2890fa6ece731822d7261826d1045f
Ivan Ilic <ivan.ilic@student.hpi.uni-potsdam.de>
2019-12-10 19:41:33 +0100

    nginx output changed

--- 2/measure-nginx.sh
+++ 2/measure-nginx.sh
@@ -3,7 +3,7 @@ SECONDS=0
 counter=0
 sum=0
 while [ $SECONDS -lt 11 ]; do
-	result=$(for i in {1..3}; do echo $1; done | xargs -P3 -n 1 curl -o /dev/null -s -w '%{time_total}#' | sed 's/,/\./g')
+	result=$(for i in {1..3}; do echo "$1/very_big_file.txt"; done | xargs -P3 -n 1 curl -o ~/Downloads/output.txt -s -w '%{time_total}#' | sed 's/,/\./g')
 	((counter++))
 	IFS='#' read -r -a array <<< "$result"
 	for element in "${array[@]}"
